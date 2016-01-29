05187eda5ff7515b203c3ce1630433ec71c6af45
nopnop2002 <hau26280@tree.odn.ne.jp>
2016-01-30 07:23:40 +0900

    ATtiny861/4313
    I added a definition of ATtiny461 and a definition of ATtiny2313.

--- tinySPI.h
+++ tinySPI.h
@@ -21,15 +21,25 @@
 
 //USI ports and pins
 #if defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
-#define SPI_DDR_PORT DDRA
-#define USCK_DD_PIN DDA4
-#define DO_DD_PIN DDA5
-#define DI_DD_PIN DDA6
+  #define SPI_DDR_PORT DDRA
+  #define USCK_DD_PIN DDA4
+  #define DO_DD_PIN DDA5
+  #define DI_DD_PIN DDA6
 #elif defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
-#define SPI_DDR_PORT DDRB
-#define USCK_DD_PIN DDB2
-#define DO_DD_PIN DDB1
-#define DI_DD_PIN DDB0
+  #define SPI_DDR_PORT DDRB
+  #define USCK_DD_PIN DDB2
+  #define DO_DD_PIN DDB1
+  #define DI_DD_PIN DDB0
+#elif defined(__AVR_ATtiny461__) || defined(__AVR_ATtiny861__)
+  #define SPI_DDR_PORT DDRB
+  #define USCK_DD_PIN DDB2
+  #define DO_DD_PIN DDB1
+  #define DI_DD_PIN DDB0
+#elif defined(__AVR_ATtiny2313__) || defined(__AVR_ATtiny4313__)
+  #define SPI_DDR_PORT DDRB
+  #define USCK_DD_PIN DDB7
+  #define DO_DD_PIN DDB6
+  #define DI_DD_PIN DDB5
 #endif
 
 //SPI data modes
